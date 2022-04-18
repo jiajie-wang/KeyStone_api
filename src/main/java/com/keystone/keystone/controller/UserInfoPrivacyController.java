82a34cacb966e4aa5ea9fab9728b366848010007
Rizzo <2391537434@qq.com>
2022-04-19 00:26:19 +0800

    add a gloss

--- src/main/java/com/keystone/keystone/controller/UserInfoPrivacyController.java
+++ src/main/java/com/keystone/keystone/controller/UserInfoPrivacyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//用户部分基本信息隐私设置
 @RestController
 public class UserInfoPrivacyController {
     @Autowired
